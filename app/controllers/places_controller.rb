e44ec4a7a01414b0e77aab7780e648a82dffb4b2
Gregory Schern <schern.gregory@gmail.com>
2015-12-31 05:12:14 +0000

    Connect place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,20 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.paginate(page: params[:page], per_page: 10)
+		@places = Place.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
 		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :address, :description)
 	end
 end
