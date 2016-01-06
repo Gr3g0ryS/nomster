ea1ea1f2e0cdaddc204fe67db15d28df0f8e2887
Gregory Schern <schern.gregory@gmail.com>
2016-01-06 20:24:44 +0000

    Add form for photos on place, connected and tested

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,14 @@
 class PhotosController < ApplicationController
 	before_action :authenticate_user!
 	def create
-		@photo = Place.find(params[:photo_id])
+		@place = Place.find(params[:place_id])
+		@place.photos.create(photo_params.merge(:user => current_user))
+		redirect_to place_path(@place)
+	end
+
+	private
+
+	def photo_params
+		params.require(:photo).permit(:caption)
 	end
 end
