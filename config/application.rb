2910315c06b67895a453fc34e8f5e79a7a2a03dd
Gregory Schern <schern.gregory@gmail.com>
2016-01-06 21:38:46 +0000

    Add photo to index

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Nomster
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
